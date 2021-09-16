d550a03a7e9bf9cf036af0bcb1b096832c2a2f08
DavidV <david.vieuxbled@outlook.fr>
2021-09-16 15:34:20 +0200

    exos_fonction2

--- js/exos_fonction_native.js
+++ js/exos_fonction_native.js
@@ -46,3 +46,78 @@ console.log("\n# Exercice 5#\n");
 var texte = "A nice boat"
 console.log(typeof texte);
 // String
+var letters = texte.split("");
+console.log(letters);
+
+var words = texte.split(" ");
+console.log(words);
+
+console.log("\n# Exercice 6#\n");
+// Exercice 6_____________________________________________________________________________________________________________________________________
+
+var name = "Paul Dumas";
+var expectedName = "paul dumas";
+if (name.toLocaleLowerCase() === expectedName) {
+  console.log("OK Paul you can come in");
+}
+else {
+  console.log("We do not know you");
+}
+//L'usage de cette fonction sur un site serait sûrement de gérer des infos irrecevable en majuscule
+// toLocaleUpperCase()
+
+console.log("\n# Exercice 7#\n");
+// Exercice 7_____________________________________________________________________________________________________________________________________
+
+var newStudent = "Ryan Oper"
+var students = [
+  "John Doe",
+  "Jane Doe",
+  "Marc Alm",
+  "Jimmy Turner"
+]
+console.log(newStudent);
+console.log(students); 
+students.push(newStudent);
+console.log(students);
+students.push("Franck Martin", "Tom Hawkins");
+console.log(students);
+
+console.log("\n# Exercice 8#\n");
+// Exercice 8____________________________________________________________________________________________________________________________________
+
+var transactions = [
+  102,
+  50,
+  4563,
+  42,
+  78,
+  146,
+  10563
+]
+
+var researched = 4563;
+console.log(transactions.includes(researched));
+// true avec 42 (et tous les nombre de la var transactions), false avec 44 (et tous les nombres absent de la var transactions)
+// La fonction includes() permet de déterminer si un tableau contient une valeur et renvoie true si c'est le cas, false sinon.
+if (transactions.includes(researched)) {
+  console.log("We found a matching transaction");
+}
+else {
+  console.log("No match");
+}
+
+console.log("\n# Exercice 9#\n");
+// Exercice 9____________________________________________________________________________________________________________________________________
+
+var students = [
+  "John Doe",
+  "Jane Doe",
+  "Marc Alm",
+  "Jimmy Turner"
+]
+console.log(students);
+students.sort();
+console.log(students);
+var lastElement = students[students.length - 1];
+console.log(lastElement);
